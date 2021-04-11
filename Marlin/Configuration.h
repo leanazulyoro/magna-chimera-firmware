5582815dc68608694bf371b38cccd3e294f28b48
Leandro Almeida <leanazulyoro@gmail.com>
2021-04-11 13:20:47 -0300

    fix bed size to 225x225

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1164,8 +1164,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 230
-#define Y_BED_SIZE 230
+#define X_BED_SIZE 225
+#define Y_BED_SIZE 225
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
